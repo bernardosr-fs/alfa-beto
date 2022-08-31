83f8e7b8977ea3ec7ea0e49cda85ae14fefb05b6
bernardo.rocha <bernardo.rocha@cwi.com.br>
2022-08-30 23:03:39 -0300

    initial-commit

---
+++ alfa-beto-api/src/main/java/com/alfabetoapi/AlfaBetoApiApplication.java
@@ -0,0 +1,13 @@
+package com.alfabetoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaBetoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaBetoApiApplication.class, args);
+	}
+
+}
---
+++ alfa-beto-api/src/test/java/com/alfabetoapi/AlfaBetoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfabetoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaBetoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
